169ba26beb9a0fd4fb971e72efb31d846d4587bf
Eli Thorkelson <eli@uchicago.edu>
2013-05-21 13:40:45 -0500

    Update rvm requirement to 1.9.3. Update sunspot requirement to 2.0.0. Update connection spec to reflect updated FakeWeb behavior

--- spec/rsolr/connection_spec.rb
+++ spec/rsolr/connection_spec.rb
@@ -10,14 +10,14 @@ describe RSolr::Connection do
                              :body => "{'result' => 'Authorized'}"
       end
 
-      let(:client) { RSolr::Client.new(RSolr::Connection.new, :url => 'http://user:pass@127.0.0.1:8983/solr') }
-      let(:result) { client.get('/select', :params => {:q => 'a'}) }
+      let(:client) { RSolr.connect(:url => 'http://user:pass@127.0.0.1:8983/solr') }
+      let(:result) { client.get('select', :params => {:q => 'a'}) }
 
       specify { result.response[:data].should be_nil }
       specify { result.response[:body].should eq("{'result' => 'Authorized'}") }
       specify { result.response[:status].should eq(200) }
-      specify { result.request[:path].should eq('/select') }
-      specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/select?q=a&wt=ruby') }
+      specify { result.request[:path].should eq('select') }
+      specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/select?wt=ruby&q=a') }
       specify { result.response[:headers].should be_empty }
       specify { result.request[:params].should eq({:q => 'a', :wt => :ruby}) }
     end
@@ -30,19 +30,19 @@ describe RSolr::Connection do
                              :status => [401, 'Unauthorized']
       end
 
-      let(:client) { RSolr::Client.new(RSolr::Connection.new, :url => 'http://user:pass@127.0.0.1:8983/solr') }
-      let(:result) { client.get('/select', :params => {:q => 'a'}) }
+      let(:client) { RSolr.connect(:url => 'http://user:pass@127.0.0.1:8983/solr') }
+      let(:result) { client.get('select', :params => {:q => 'a'}) }
 
       specify { result.response[:data].should be_nil }
       specify { result.response[:body].should eq('Unauthorized') }
       specify { result.response[:status].should eq(401) }
-      specify { result.request[:path].should eq('/select') }
-      specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/select?q=a&wt=ruby') }
+      specify { result.request[:path].should eq('select') }
+      specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/select?wt=ruby&q=a') }
       specify { result.response[:headers].should be_empty }
       specify { result.request[:params].should eq({:q => 'a', :wt => :ruby}) }
     end
   end
-  
+
   describe '#post' do
     context 'with valid basic auth credentials' do
       before do
@@ -50,19 +50,19 @@ describe RSolr::Connection do
                              'http://user:pass@127.0.0.1:8983/solr/update?wt=ruby',
                              :body => "{'result' => 'Authorized'}"
       end
-  
-      let(:client) { RSolr::Client.new(RSolr::Connection.new, :url => 'http://user:pass@127.0.0.1:8983/solr') }
-      let(:result) { client.post('/update', :data => '<rollback/>') }
-  
+
+      let(:client) { RSolr.connect(:url => 'http://user:pass@127.0.0.1:8983/solr') }
+      let(:result) { client.post('update', :data => '<rollback/>') }
+
       specify { result.request[:data].should eq('<rollback/>') }
       specify { result.response[:body].should eq("{'result' => 'Authorized'}") }
       specify { result.response[:status].should eq(200) }
-      specify { result.request[:path].should eq('/update') }
+      specify { result.request[:path].should eq('update') }
       specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/update?wt=ruby') }
       specify { result.response[:headers].should be_empty }
       specify { result.request[:params].should eq({:wt => :ruby}) }
     end
-  
+
     context 'with invalid basic auth credentials' do
       before do
         FakeWeb.register_uri :post,
@@ -71,13 +71,13 @@ describe RSolr::Connection do
                              :status => [401, 'Unauthorized']
       end
 
-      let(:client) { RSolr::Client.new(RSolr::Connection.new, :url => 'http://user:pass@127.0.0.1:8983/solr') }
-      let(:result) { client.post('/update', :data => '<rollback/>') }
+      let(:client) { RSolr.connect(:url => 'http://user:pass@127.0.0.1:8983/solr') }
+      let(:result) { client.post('update', :data => '<rollback/>') }
 
       specify { result.request[:data].should eq('<rollback/>') }
       specify { result.response[:body].should eq('Unauthorized') }
       specify { result.response[:status].should eq(401) }
-      specify { result.request[:path].should eq('/update') }
+      specify { result.request[:path].should eq('update') }
       specify { result.request[:uri].to_s.should eq('http://user:pass@127.0.0.1:8983/solr/update?wt=ruby') }
       specify { result.response[:headers].should be_empty }
       specify { result.request[:params].should eq({:wt => :ruby}) }
--- sunspot-rails-http-basic-auth.gemspec
+++ sunspot-rails-http-basic-auth.gemspec
@@ -3,7 +3,7 @@ $:.push File.expand_path('../lib', __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = 'sunspot-rails-http-basic-auth'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.platform    = Gem::Platform::RUBY
   s.author      = 'Justin Ko'
   s.email       = 'jko170@gmail.com'
@@ -15,9 +15,9 @@ Gem::Specification.new do |s|
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_path  = 'lib'
-  
-  s.add_dependency 'sunspot_rails', '~> 1.3.0'
-  
+
+  s.add_dependency 'sunspot_rails', '~> 2.0.0'
+
   s.add_development_dependency 'rspec', '~> 2.5'
   s.add_development_dependency 'fakeweb'
 end
