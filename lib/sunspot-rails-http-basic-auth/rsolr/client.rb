d8b4069568be1863d04704af3fbba874167a9dee
James Wachira <jamomathenge@yahoo.com>
2014-06-20 16:47:59 -0700

    -code cleanup -bump gem versions

--- lib/sunspot-rails-http-basic-auth/rsolr/client.rb
+++ lib/sunspot-rails-http-basic-auth/rsolr/client.rb
@@ -1,16 +1,18 @@
-class RSolr::Client
-  alias_method :adapt_response_original, :adapt_response
+module RSolr
+  class Client
+    alias_method :adapt_response_original, :adapt_response
 
-  def adapt_response request, response
-    if response[:status] == 401
-      result = response[:body]
-      result.extend Context
-      result.request = request
-      result.response = response
-      return result
-    else
-      adapt_response_original(request, response)
+    def adapt_response request, response
+      if response[:status] == 401
+        result = response[:body]
+        result.extend Context
+        result.request = request
+        result.response = response
+        return result
+      else
+        adapt_response_original(request, response)
+      end
     end
-  end
 
+  end
 end
--- lib/sunspot-rails-http-basic-auth/rsolr/connection.rb
+++ lib/sunspot-rails-http-basic-auth/rsolr/connection.rb
@@ -1,12 +1,14 @@
-class RSolr::Connection
-  alias_method :setup_raw_request_original, :setup_raw_request
+module RSolr
+  class Connection
+    alias_method :setup_raw_request_original, :setup_raw_request
 
-  def setup_raw_request request_context
-    raw_request = setup_raw_request_original(request_context)    
-    if request_context[:uri].user and request_context[:uri].password
-      raw_request.basic_auth(request_context[:uri].user, request_context[:uri].password) 
+    def setup_raw_request request_context
+      raw_request = setup_raw_request_original(request_context)
+      if request_context[:uri].user and request_context[:uri].password
+        raw_request.basic_auth(request_context[:uri].user, request_context[:uri].password)
+      end
+      raw_request
     end
-    raw_request
-  end
 
+  end
 end
